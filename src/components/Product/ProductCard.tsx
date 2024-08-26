edf815eef9570c2dfeef7e1a99e7d29311561f4c
WilloS88 <ramon2003@email.cz>
2024-08-26 22:03:02 +0200

    error handle

--- src/components/Product/ProductCard.tsx
+++ src/components/Product/ProductCard.tsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 import { DollarSign } from "lucide-react";
-import { Product } from "../../types/Product";
 import { Card } from "../ui/Card";
+import type { Product } from "../../types/Product";
 
 type ProductCardProps = {
   product: Product;
@@ -11,12 +11,12 @@ export const ProductCard = ({ product }: ProductCardProps) => {
   return (
     <Link to={`/products/${product.id}`}>
       <Card>
-        <img className="w-full" src={product.image} alt={product.name} />
+        <img className="w-full" src={product.image} alt={product.name}/>
         <div className="px-6 py-4">
           <div className="font-bold text-xl mb-2">{product.name}</div>
           <p className="text-gray-700 text-base">{product.description}</p>
           <div className="flex items-center">
-            <DollarSign size={14} strokeWidth={3} className="mr-1" />
+            <DollarSign size={14} strokeWidth={3} className="mr-1"/>
             <span className="text-gray-900 font-bold">{product.price}</span>
           </div>
           <p className="text-gray-600">Type: {product.type}</p>
