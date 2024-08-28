8a72317768b07395dbe707a2da2df8ab172ccdff
WilloS88 <ramon2003@email.cz>
2024-08-28 21:45:52 +0200

    Error fix try #4

---
+++ src/components/product/ProductSwiper.tsx
@@ -0,0 +1,24 @@
+import "swiper/css";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { products } from "../../data/products";
+import { ProductCard } from "./ProductCard";
+
+const ProductSwiper: React.FC = () => {
+  return (
+    <Swiper
+      spaceBetween={10}
+      slidesPerView={3}
+      navigation
+      pagination={{ clickable: true }}
+      autoplay={{ delay: 3000 }}
+    >
+      {products.slice(0, 5).map((product) => (
+        <SwiperSlide key={product.id}>
+          <ProductCard product={product} />
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  );
+};
+
+export default ProductSwiper;
